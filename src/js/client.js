368da06d6362693c8a4240a0cba624da321c4803
Unknown <dia151378@uvg.edu.gt>
2018-07-16 15:10:15 -0300

    subiendo js e imagenes

--- src/js/client.js
+++ src/js/client.js
@@ -1,108 +1,150 @@
-import expect from 'expect';
-import { createStore } from 'redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Immutable from 'immutable';
-
-// Javascript the good parts
-
-const Counter = ({ value, incrementAction, decrementAction, removeAction }) => (
-  <div>
-    <h1>{ value }</h1>
-    <button onClick={ incrementAction }>+</button>
-    <button onClick={ decrementAction }>-</button>
-    <button onClick={ removeAction }>x</button>
-  </div>
-);
-
-const CounterList = ({ list }) => (
-  <div>
-    {
-      list.map(
-        (value, i) => (
-          <Counter
-            key={ i }
-            value={ value }
-            index={ i }
-            incrementAction={
-              () => store.dispatch({
-                type: 'INCREMENT',
-                payload: { index: i }
-              })
-            }
-            decrementAction={
-              () => store.dispatch({
-                type: 'DECREMENT',
-                payload: { index: i }
-              })
-            }
-            removeAction={
-              () => store.dispatch({
-                type: 'REMOVE_COUNTER',
-                payload: {
-                  index: i
-                }
-              })
-            }
-          />
-        )
-      )
+//Ana lucia Diaz 
+//151378
+//pinta la diagonal
+const verganadordiagonal = (matriz) => {
+    for(var i=0; i<3; i++){
+        matriz[i][3-1-i] = 1; 
+        ganadora = matriz[i][0];
     }
-    <button onClick={ () => store.dispatch({ type: 'ADD_COUNTER' }) }>Add counter</button>
-  </div>
-);
-
-const validateIndex = (index, list) => 0 <= index && index < list.size;
-
-// Reducer
-const counterList = (state = Immutable.List.of(), action) => {
-
-  if(typeof action.payload !== 'undefined'){
-    var { index } = action.payload;
-  }
-
-  switch(action.type){
-    case 'ADD_COUNTER':
-      return state.push(0);
-
-    case 'REMOVE_COUNTER':
-
-      if(validateIndex(index, state)){
-        return state.delete(index);
-      }
-
-      return state;
-
-    case 'INCREMENT':
-
-      if(validateIndex(index, state)){
-        return state.update(index, (v) => v + 1);
-      }
-
-      return state;
-
-    case 'DECREMENT':
-
-      if(validateIndex(index, state)){
-        return state.update(index,  (v) => v - 1);
-      }
-
-      return state;
-
-    default:
-      return state;
-  }
+    for (var i=0; i<3 ; i++){
+        for (var j=0; j <3 ; j++){
+            (matriz[i][j]);
+            ganadora = matriz[j][0]; 
+        }
+    }
 }
 
-// createStore: reducer --> store
-const store = createStore(counterList);
+//const verganadordiagonal = (matriz) => {
+//    matriz.forEach(
+//        (column, matriz) => {
+//            diagonal(matriz);
+//        }
+//    )
+//}
 
-const render = () => {
-  ReactDOM.render(
-    <CounterList list={ store.getState() } />,
-    document.getElementById('root')
-  )
+//chequea que exista un ganador en la fila respectiva
+const verganadorfila = (matriz) => {
+    matriz.forEach(
+        (column, colIndice) => {
+            columna(colIndice, matriz);
+        }
+    )
 }
+//chequea para ver que exista un ganador en la columna respectiva
+const verganadorcolumna = (matriz) => {
+    matriz.forEach(
+        (column, filaI) => {
+            fila(filaI, matriz);
+        }
+    )
+}
+//chequea para ver que exista un ganar en diagonal 
+//se define el estado inicial
+const EstadoInicial = (len) => {
+    return ({
+        arreglodelmapa: Array.from({length: len}, (x, i) => Array.from({length: len}, (y, j) => null)),
+        ganadora: null,
+        jugadoractual: 0
+       
+    });
+};
+const columna = (colIndice, matriz) => {
+  const memoria = matriz.map((column) => {
+      return column[colIndice]
+  });
+  console.log(memoria);
 
-store.subscribe(render);
-render();
+  if (memoria.reduce((a, b) => {
+          return (a === b) ? a : null;
+      }) !== null) {
+      ganadora = memoria[0];
+  }
+};
+const fila = (filaI, matriz) => {
+    console.log(matriz[filaI]);
+    if (!matriz[filaI].includes(null)) {
+        if (matriz[filaI].reduce((a, b) => {
+                return (a === b) ? a : null;
+            }) !== null) {
+            ganadora = matriz[filaI][0];
+        }
+    }
+};
+
+
+let ganadora = null;
+
+const render = (lState) => {
+    const ContenedorDeJuego = document.createElement('div');
+    ContenedorDeJuego.className = 'jugador';
+    ContenedorDeJuego.innerHTML = 'TURNO DE:  ';
+    const totito = document.createElement('div');
+    totito.className = 'Totito';
+    const nuevo = document.createElement('button');
+    nuevo.className= ' nuevo';
+    nuevo.innerHTML = 'REINICIAR JUEGO';
+    const imagendeturno = document.createElement(`img`);
+    imagendeturno.src = (lState.jugadoractual) ? "../img/firstplayer.JPG": "../img/secondplayer.JPG";
+    ContenedorDeJuego.appendChild(imagendeturno);
+    
+    
+
+    // Propiedad para borrar
+    if (root.hasChildNodes()) {
+        root.innerHTML = null;
+    }
+
+    //creacion de containers
+    root.appendChild(totito);
+    root.appendChild(ContenedorDeJuego);
+    root.appendChild(nuevo);
+    
+
+    
+    lState.arreglodelmapa.forEach(
+        (column, filaI) => {
+            //se crea el div del tamano definido
+            const rowElement = document.createElement('div');
+            column.forEach(
+                (col, colIndice) => {
+                    //se crea el div del tablero y se definen las lineas
+                    const tablero = document.createElement('div');
+                    if (ganadora === null) {
+                      tablero.onclick = () => {
+                          //se crea la opcion de x y y, para el cambio de variables
+                            if (lState.arreglodelmapa[filaI][colIndice] === null) {
+                                //se define el div.o y div.x para sacar la imagen
+                                tablero.classList.add((lState.jugadoractual) ? 'o' : 'x');
+                                lState.arreglodelmapa[filaI][colIndice] = lState.jugadoractual;
+                                lState.jugadoractual = (lState.jugadoractual === 0) ? 1 : 0;
+                                render(lState); //valida el cambio de x a y 
+                                verganadorfila(lState.arreglodelmapa);
+                                verganadorcolumna(lState.arreglodelmapa);
+                                //verganadordiagonal(lState.arreglodelmapa);
+
+                    
+                            } 
+                        };
+                    }
+                    if (lState.arreglodelmapa[filaI][colIndice] !== null) {
+                        tablero.classList.add((lState.arreglodelmapa[filaI][colIndice]) ? 'o' : 'x');
+                      }
+                    rowElement.appendChild(tablero);
+                }
+            );
+            totito.appendChild(rowElement);
+        }
+    );
+    if (ganadora!== null){
+        alert ("ya han ganado el juego");
+        
+
+    }
+    nuevo.onclick = () => {
+        ganadora = null; 
+        render(EstadoInicial(lState.arreglodelmapa.length));
+        alert("SE A REINICIADO EL JUEGO");
+      };
+    
+};
+render(EstadoInicial(3));
---
+++ src/js/client2.js
@@ -0,0 +1,108 @@
+const state = {
+    lights: ['red', 'yellow', 'green','red1', 'yellow1', 'green1','red2', 'yellow2', 'green2','seleccionado'],
+    currentLight: 0,
+  };
+  const buildInitialState = (len) => {
+    return ({
+        statusMap: Array.from({length: len}, (x, i) => Array.from({length: len}, (y, j) => null)),
+        currentPlayer: 0,
+        winner: null
+    });
+  };
+  const render = lState => {
+    const title = document.createElement('h1');
+    title.innerHTML = 'TOTITO';
+  
+    const totito = document.createElement('div');
+    totito.className = 'totito';
+  
+    const nextBtn = document.createElement('button');
+    nextBtn.className = 'nextBtn';
+    nextBtn.innerHTML = 'siguiente posicion';
+  
+    const aceptarturno = document.createElement('button');
+    aceptarturno.className = 'aceptarturno';
+    aceptarturno.innerHTML = 'seleccionar';
+  
+    const nuevo = document.createElement('button');
+    nuevo.className = 'nuevo';
+    nuevo.innerHTML = 'siguiente turno';
+  
+    const turnoContainer = document.createElement('div');
+    turnoContainer.className = 'turno';
+    turnoContainer.innerHTML = 'es el turno de:';
+  
+    const turnoIndicator = document.createElement('img');
+      turnoIndicator.src = (lState.currentPlayer) ? "../img/X.png" : "../img/O.png";
+      turnoContainer.appendChild(turnoIndicator);
+  
+  
+  
+    // Clear previous root content
+    if (root.hasChildNodes()) {
+      root.innerHTML = null;
+    }
+  
+    // Main rendering
+    root.appendChild(title);
+    root.appendChild(totito);
+    root.appendChild(nextBtn);
+    root.appendChild(aceptarturno);
+    root.appendChild(nuevo);
+    root.appendChild(turnoContainer);
+  
+    const lightElements = lState.lights.map(
+      (lightColor, i, j) => {
+        const lightElement = document.createElement('div');
+        lightElement.className = `light ${lightColor}`;
+  
+        if (i === lState.currentLight) {
+          lightElement.classList.add('on');
+          
+        }
+        
+        //if (i !== j) {
+        //  lightElement.classList.add('off');
+        //  if (j === lState.currentLight) {
+        //    lightElement.classList.add('off');
+        //  }
+        //}
+       
+      
+      
+  
+        return lightElement;
+      }
+    );
+  
+    
+  
+    
+   
+  
+   
+  
+    lightElements.forEach(
+      lightElement => totito.appendChild(lightElement)
+    );
+  
+  
+    // Events
+    nextBtn.onclick = () => {
+      lState.currentLight = (lState.currentLight + 1) % lState.lights.length;
+      render(lState);
+    };
+    aceptarturno.onclick = () => {
+      lState.currentLight = (lState.currentLight) % lState.lights.length;
+      render(lState);
+    };
+    nuevo.onclick = () => {
+      lState.currentLight = (lState.currentLight + 1) % lState.lights.length;
+      render(lState);
+    };
+  }
+    
+  
+  
+  render(state);
+  
